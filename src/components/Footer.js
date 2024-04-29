a26b06081899c12f387b354253f21f8c15e065bf
Dominique Paul <dominique.c.a.paul@gmail.com>
2024-04-29 12:31:39 +0200

    Update title

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ export default class Footer extends React.Component {
 
         return(
             <footer className="footer-gradient logos fixed bottom-0 left-0 z-20 w-full h-16 p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
-                <span className="text-sm text-gray-800 sm:text-center dark:text-gray-400">© 2023 <a href="/" className="hover:underline">Dominique Paul</a>. All Rights Reserved.</span>
+                <span className="text-sm text-gray-800 sm:text-center dark:text-gray-400">© 2024 <a href="/" className="hover:underline">Dominique Paul</a>. All Rights Reserved.</span>
                 <ul className="flex flex-wrap items-center text-sm text-gray-800 dark:text-gray-400 sm:mt-0">
                     <li>
                         <a className="mr-4 hover:underline md:mr-6"  target="_blank" rel="noopener noreferrer" href="https://thisiscrispin.substack.com" onClick={this.gaEventTracker("Clicked", "substack-link-footer")}>Substack</a>
