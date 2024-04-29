84f23ddec8467762543df1012fa33a90a65bebd2
Dominique Paul <dominique.c.a.paul@gmail.com>
2024-04-29 15:26:03 +0200

    Add image

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Heading = ({title}) => {
     return ( 
-        <div className="pt-28 pb-48 pl-[25%] short-bottom ">
+        <div className="pt-28 pl-[25%] short-bottom ">
             <div className="heading central">
                 <h3 className="text-lg font-medium"><a href="/">thisiscrispin</a></h3>
                 <h1 className="text-5xl font-bold pb-2">{ title }</h1>
